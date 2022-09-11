2a150cd533b3b55f63661a9b6c7c74f9486f9098
AdamcyNaira <adammusa89@gmail.com>
2022-09-11 20:51:09 +0100

    working on recommendation

--- lib/ui/book_details.dart
+++ lib/ui/book_details.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import '../widgets/client_widgets.dart';
 
 class BooksDetails extends StatefulWidget {
@@ -24,26 +24,11 @@ class _BooksDetailsState extends State<BooksDetails> {
       ),
       body: ListView(
         children: [
-          //   RatingBar.builder(
-          //   initialRating: 3,
-          //   minRating: 1,
-          //   direction: Axis.horizontal,
-          //   allowHalfRating: true,
-          //   itemCount: 5,
-          //   itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-          //   itemBuilder: (context, _) => Icon(
-          //     Icons.star,
-          //     color: Colors.amber,
-          //   ),
-          //   onRatingUpdate: (rating) {
-          //     print(rating);
-          //   },
-          // ),
           detailCard(
             context: context,
             title: widget.title,
             author: widget.author,
-            img: "images/book.jpg",
+            img: "images/beach.jpeg",
             description: widget.description,
           ),
         ],
